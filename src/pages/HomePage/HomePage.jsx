f07cae71bc990eede7bed6eeffe4f7a1784aa3b8
Ekene-Ifiora <ekeneifiora@gmail.com>
2024-02-08 21:15:57 -0600

    Working login page with additional file for homepage and logout functionality

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,31 @@
 import { Box, Container, Flex } from "@chakra-ui/react";
 import FeedPosts from "../../components/FeedPosts/FeedPosts";
-
-
+import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers";
+import Sidebar from "../../components/Sidebar/Sidebar";
 const HomePage = () => {
 	return (
 		<Container maxW={"container.lg"}>
 			<Flex gap={20}>
-				<Box flex={2} py={10}>
-				<FeedPosts />
+			<Box w={{ base: "70px", md: "240px" }}>
+					<Sidebar />
 				</Box>
 				<Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
 					
 				</Box>
 			</Flex>
 		</Container>
+
+
+	 // <Container maxW={"container.lg"}>
+		// 	<Flex gap={20}>
+		// 	<Box w={{ base: "70px", md: "240px" }}>
+		// 			<Sidebar />
+		// 		</Box>
+		// 		<Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
+					
+		// 		</Box>
+		// 	</Flex>
+		// </Container> 
 	);
 };
 
