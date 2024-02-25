6b4585ff2c6a68a91abc86ef395c829147e44109
MarcusKamen <marcus.p.kamen@vanderbilt.edu>
2024-02-25 12:24:25 -0600

    test github actions

--- backend/test/backend.test.js
+++ backend/test/backend.test.js
@@ -6,7 +6,6 @@ import app from '../src/backend.js';
 describe('API tests', () => {
     it('Hello World', async () => {
         const response = await supertest(app).get('/');
-
         expect(response.status).to.equal(200)
         expect(response.body.message).to.equal('Hello World')
     })
