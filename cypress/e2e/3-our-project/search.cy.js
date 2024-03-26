3425d23f5262e4c455bc6dc14c76cbe6e1ea6863
MarcusKamen <marcus.p.kamen@vanderbilt.edu>
2024-03-25 21:21:17 -0500

    hopefully fixed actions

--- cypress/e2e/3-our-project/search.cy.js
+++ cypress/e2e/3-our-project/search.cy.js
@@ -2,34 +2,25 @@ describe('search tests', () => {
   it('passes', () => {
     cy.visit('/')
   })
-
-  /* ==== Test Created with Cypress Studio ==== */
-  it('Search Click and Out', function() {
+  
+  it('search good', () => {
     /* ==== Generated with Cypress Studio ==== */
     cy.visit('/');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').clear('m');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').type('marcus.p.kamen@vanderbilt.edu');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').clear('P');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').type('Password');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > .sc-gLLuof').click();
     cy.get('.search-input').click();
-    cy.get('.chakra-modal__close-btn').click();
-    cy.get('.search-input').click();
-    cy.get('.chakra-modal__content-container').click();
-    /* ==== End Cypress Studio ==== */
-  });
-
-  /* ==== Test Created with Cypress Studio ==== */
-  it('Search Item', function() {
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('/');
-    cy.get('.search-input').click();
-    cy.get('#field-\\:r17\\:').clear('Headphones');
+    cy.get('#field-\\:r17\\:').clear('H');
     cy.get('#field-\\:r17\\:').type('Headphones');
     cy.get('.css-117fnea').click();
     cy.get('.chakra-button').click();
-    cy.get('.css-117fnea').click();
-    cy.get(':nth-child(2) > .css-1k9efnl > .chakra-stack > button > .css-1668g05').click();
+    cy.get(':nth-child(2) > .css-1k9efnl > :nth-child(1) > .chakra-avatar > .chakra-avatar__img').click();
     /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('Failed Search', function() {
+  it('search bad', () => {
     /* ==== Generated with Cypress Studio ==== */
     cy.visit('/');
     cy.get('.search-input').click();
@@ -37,52 +28,32 @@ describe('search tests', () => {
     cy.get('#field-\\:r17\\:').type('h');
     cy.get('.css-117fnea').click();
     cy.get('.chakra-button').click();
-    cy.get('#toast-1-title').click();
-    cy.get('.css-117fnea').click();
+    cy.get('#toast-1-description').click();
     cy.get('.css-1pq15d > .chakra-icon').click();
-    cy.get('.css-117fnea').click();
-    cy.get('.chakra-modal__close-btn').click();
     /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('Location Search', function() {
+  it('search location', () => {
     /* ==== Generated with Cypress Studio ==== */
     cy.visit('/');
     cy.get('.search-input').click();
-    cy.get('#field-\\:r17\\:').clear('m');
-    cy.get('#field-\\:r17\\:').type('moore{enter}');
-    cy.get('.chakra-button').click();
-    cy.get('#toast-1-title').click();
-    cy.get('#chakra-modal--body-\\:r7\\:').click();
-    cy.get('#field-\\:r17\\:').clear();
-    cy.get('#field-\\:r17\\:').type('warren{enter}');
-    cy.get('.chakra-button').click();
-    cy.get('#toast-2-description').click();
-    cy.get('#chakra-modal--body-\\:r7\\:').click();
-    cy.get('#field-\\:r17\\:').clear();
+    cy.get('#field-\\:r17\\:').clear('w');
     cy.get('#field-\\:r17\\:').type('Warren{enter}');
     cy.get('.chakra-button').click();
-    cy.get('.css-117fnea').click();
-    cy.get('.css-1668g05').click();
+    cy.get('.chakra-stack').click();
+    cy.get('.chakra-avatar__img').click();
     /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('Type Based Search', function() {
+  it('search category', () => {
     /* ==== Generated with Cypress Studio ==== */
     cy.visit('/');
     cy.get('.search-input').click();
-    cy.get('#field-\\:r17\\:').clear('el');
-    cy.get('#field-\\:r17\\:').type('elec{enter}');
-    cy.get('.chakra-button').click();
-    cy.get('#toast-1-title').click();
-    cy.get('#chakra-modal--body-\\:r7\\:').click();
-    cy.get('#field-\\:r17\\:').clear();
+    cy.get('#field-\\:r17\\:').clear('E');
     cy.get('#field-\\:r17\\:').type('Electronics{enter}');
     cy.get('.chakra-button').click();
-    cy.get('.css-117fnea').click();
-    cy.get(':nth-child(2) > .css-1k9efnl > .chakra-stack > button > .css-1668g05').click();
+    cy.get(':nth-child(2) > .css-1k9efnl > :nth-child(1) > .chakra-avatar > .chakra-avatar__img').click();
     /* ==== End Cypress Studio ==== */
   });
-})
+  
+});
