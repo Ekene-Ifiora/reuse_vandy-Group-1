bda3c900ede640c8f646743bdd8b46373d060ead
MarcusKamen <marcus.p.kamen@vanderbilt.edu>
2024-03-27 12:51:19 -0500

    a few more tests written

--- src/hooks/tests/useAddCart.test.js
+++ src/hooks/tests/useAddCart.test.js
@@ -10,7 +10,7 @@ import { ConstructionOutlined } from '@mui/icons-material';
 jest.mock('firebase/firestore');
 jest.mock('../../store/authStore');
 jest.mock('../../firebase/firebase');
-jest.mock('../useShowToast')
+jest.mock('../useShowToast');
 
 query.mockResolvedValue({});
 getDocs.mockResolvedValue({});
@@ -20,13 +20,19 @@ collection.mockResolvedValue({});
 arrayUnion.mockResolvedValue({});
 doc.mockResolvedValue({});
 updateDoc.mockResolvedValue({});
-useAuthStore.mockResolvedValue({});
+useAuthStore.mockReturnValue(true);
 
 var e1;
 var e2;
 var e3;
-useShowToast.mockImplementation(() => {const showToast = (error1, error2, error3) => {e1 = error1; e2 = error2; e3 = error3;}
-                                      return showToast;});
+useShowToast.mockImplementation(() => {
+  const showToast = (error1, error2, error3) => {
+    e1 = error1; 
+    e2 = error2; 
+    e3 = error3;
+  }
+  return showToast;
+});
 
 describe('useAddCart', () => {
   it('should initialize with isUpdating set to false', () => {
@@ -34,14 +40,13 @@ describe('useAddCart', () => {
     expect(result.current.isUpdating).toBe(false);
   });
 
-  it('should add to cart successfully', async () => {
+  it('should run handleAddCart successfully', async () => {
     const { result } = renderHook(() => useAddCart());
     const { handleAddCart } = result.current;
 
     await act(async () => {
       // Mock the implementation of handleAddCart for successful update
       jest.spyOn(result.current, 'handleAddCart').mockResolvedValueOnce();
-
       // Call the handleAddCart function
       await handleAddCart();
     });
@@ -66,12 +71,12 @@ describe('useAddCart', () => {
     expect(result.current.isUpdating).toBe(false);
   });
 
-  it('should add to cart with post', async () => {
+  it('should not add when item in cart already', async () => {
     const { result } = renderHook(() => useAddCart());
     const { handleAddCart } = result.current;
 
     await act(async () => {
-      var spy = jest.spyOn(result.current, 'handleAddCart');
+      jest.spyOn(result.current, 'handleAddCart');
       getDocs.mockResolvedValue([{ id: 1 }, { id: 2 }]);
 
       const data = () => {
@@ -82,11 +87,99 @@ describe('useAddCart', () => {
 
       const post = { id: 1 };
       await handleAddCart(post);
+    });
 
-      expect(e1).toBe('Info');
-      expect(e2).toBe("Item already exists in the cart");
-      expect(e3).toBe("info");
-    })
+    expect(e1).toBe('Info');
+    expect(e2).toBe("Item already exists in the cart");
+    expect(e3).toBe("info");
+
+    expect(result.current.isUpdating).toBe(false);
+  });
+
+  it('should not add when query snapshot empty', async () => {
+    const { result } = renderHook(() => useAddCart());
+    const { handleAddCart } = result.current;
+
+    await act(async () => {
+      jest.spyOn(result.current, 'handleAddCart');
+      getDocs.mockResolvedValue({ empty: true });
+
+      const post = { id: 1 };
+      await handleAddCart(post);
+    });
+
+    expect(e1).toBe('Error');
+    expect(e2).toBe("User not found");
+    expect(e3).toBe("error");
+
+    expect(result.current.isUpdating).toBe(false);
+  });
+
+  it('should add to cart sucessfully', async () => {
+    const { result } = renderHook(() => useAddCart());
+    const { handleAddCart } = result.current;
+
+    await act(async () => {
+      jest.spyOn(result.current, 'handleAddCart');
+      getDocs.mockResolvedValue([{ id: 1 }, { id: 2 }]);
+
+      const data = () => {
+        return { x: [5] }
+      };
+
+      getDoc.mockResolvedValue({data});
+
+      const post = { id: 8 };
+      await handleAddCart(post);
+    });
+
+    expect(e1).toBe('Error');
+    expect(e2).toBe("Cannot read properties of undefined (reading 'includes')");
+    expect(e3).toBe("error");
+
+    expect(result.current.isUpdating).toBe(false);
+  });
+
+  it('should produce correct error when error in doc', async () => {
+    const { result } = renderHook(() => useAddCart());
+    const { handleAddCart } = result.current;
+
+    await act(async () => {
+      jest.spyOn(result.current, 'handleAddCart');
+      getDocs.mockResolvedValue([{ id: 1 }, { id: 2 }]);
+
+      const data = () => {
+        return { cart: [5] }
+      };
+
+      getDoc.mockResolvedValue({data});
+
+      const post = { id: 8 };
+      await handleAddCart(post);
+    });
+
+    expect(e1).toBe('Success');
+    expect(e2).toBe("Added to Cart Successfully");
+    expect(e3).toBe("success");
+
+    expect(result.current.isUpdating).toBe(false);
+  });
+
+  it('should not work if not authorized', async () => {
+    useAuthStore.mockReturnValueOnce(false);
+    const { result } = renderHook(() => useAddCart());
+    const { handleAddCart } = result.current;
+
+    await act(async () => {
+      jest.spyOn(result.current, 'handleAddCart');
+
+      const post = { id: 8 };
+      await handleAddCart(post);
+    });
+
+    expect(e1).toBe('Error');
+    expect(e2).toBe("You must be logged in to add to cart");
+    expect(e3).toBe("error");
 
     expect(result.current.isUpdating).toBe(false);
   });
--- src/hooks/tests/useEditProfile.test.js
+++ src/hooks/tests/useEditProfile.test.js
@@ -1,42 +1,101 @@
 // useEditProfile.test.js
+// Import necessary dependencies and hooks from React and Firebase
+import useShowToast from "../useShowToast";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import { firestore, storage } from "../../firebase/firebase";
+import { doc, updateDoc } from "firebase/firestore";
+import useUserProfileStore from "../../store/userProfileStore";
 import { renderHook, act } from '@testing-library/react';
 import useEditProfile from '../useEditProfile';
+import useAuthStore from "../../store/authStore";
+
+jest.mock('firebase/firestore');
+jest.mock('../../store/authStore');
+jest.mock('../../firebase/firebase');
+jest.mock('../useShowToast');
+jest.mock('firebase/storage');
+jest.mock('../../store/userProfileStore');
+
+ref.mockResolvedValue({});
+doc.mockResolvedValue({});
+updateDoc.mockResolvedValue({});
+getDownloadURL.mockResolvedValue({});
+uploadString.mockResolvedValue({});
+useAuthStore.mockReturnValue((_) => { return true; });
+useUserProfileStore.mockReturnValue((_) => { return true; });
+
+var e1;
+var e2;
+var e3;
+useShowToast.mockImplementation(() => {
+  const showToast = (error1, error2, error3) => {
+    e1 = error1; 
+    e2 = error2; 
+    e3 = error3;
+  }
+  return showToast;
+});
 
 describe('useEditProfile', () => {
-  it('should initialize with isUpdating set to false', () => {
+  // it('should initialize with isUpdating set to false', () => {
+  //   const { result } = renderHook(() => useEditProfile());
+  //   expect(result.current.isUpdating).toBe(false);
+  // });
+
+  // it('should run successfully', async () => {
+  //   const { result } = renderHook(() => useEditProfile());
+  //   const { editProfile } = result.current;
+
+  //   await act(async () => {
+  //     // Mock the implementation of editProfile for successful update
+  //     jest.spyOn(result.current, 'editProfile').mockResolvedValueOnce();
+
+  //     // Call the editProfile function
+  //     await editProfile();
+  //   });
+
+  //   // After the update, isUpdating should be set to false
+  //   expect(result.current.isUpdating).toBe(false);
+  // });
+
+  // it('should handle errors during profile update', async () => {
+  //   const { result } = renderHook(() => useEditProfile());
+  //   const { editProfile } = result.current;
+
+  //   await act(async () => {
+  //     // Mock the implementation of editProfile to throw an error
+  //     jest.spyOn(result.current, 'editProfile').mockRejectedValueOnce(new Error('Update failed'));
+
+  //     // Call the editProfile function
+  //     await editProfile();
+  //   });
+
+  //   // After the error, isUpdating should be set to false
+  //   expect(result.current.isUpdating).toBe(false);
+  // });
+
+  it('should edit successfully', async () => {
     const { result } = renderHook(() => useEditProfile());
-    expect(result.current.isUpdating).toBe(false);
-  });
-
-  it('should update profile successfully', async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useEditProfile());
     const { editProfile } = result.current;
 
     await act(async () => {
-      // Mock the implementation of editProfile for successful update
-      jest.spyOn(result.current, 'editProfile').mockResolvedValueOnce();
+      jest.spyOn(result.current, 'editProfile');
 
-      // Call the editProfile function
-      await editProfile();
+      const inputs = {
+        fullname: "n",
+        username: "nm",
+        bio: "i want"
+      }
+
+      const selectedFile = true;
+
+      await editProfile(inputs, selectedFile);
     });
 
-    // After the update, isUpdating should be set to false
-    expect(result.current.isUpdating).toBe(false);
-  });
+    expect(e1).toBe('Success');
+    expect(e2).toBe("Profile updated successfully");
+    expect(e3).toBe("success");
 
-  it('should handle errors during profile update', async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useEditProfile());
-    const { editProfile } = result.current;
-
-    await act(async () => {
-      // Mock the implementation of editProfile to throw an error
-      jest.spyOn(result.current, 'editProfile').mockRejectedValueOnce(new Error('Update failed'));
-
-      // Call the editProfile function
-      await editProfile();
-    });
-
-    // After the error, isUpdating should be set to false
     expect(result.current.isUpdating).toBe(false);
   });
 });
