4b97ccef1106232530f80aaaefca924cc41f5913
MarcusKamen <marcus.p.kamen@vanderbilt.edu>
2024-03-30 17:46:20 -0500

    fixed

--- src/components/SuggestedUsers/SuggestedProduct.jsx
+++ src/components/SuggestedUsers/SuggestedProduct.jsx
@@ -1,59 +1,55 @@
-// import { Avatar, Box, Flex, VStack } from "@chakra-ui/react";
-// import ProductInfo from "../ProductPosts/ProductInfo";
-// import { React, useState } from "react";
+import { Avatar, Box, Flex, VStack } from "@chakra-ui/react";
+import ProductInfo from "../ProductPosts/ProductInfo";
+import { React, useState } from "react";
 
-import { React } from 'react';
+const SuggestedUser = ({ items, setItem }) => {
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
-const SuggestedUser = () => {
-// const SuggestedUser = ({ items, setItem }) => {
-  // const [selectedProduct, setSelectedProduct] = useState(null);
-
-  // // Iterate over each item in the items array and render its details
-  // const renderedItems = items.map((item) => (
-  //   <Flex
-  //     key={item.id}
-  //     justifyContent={"space-between"}
-  //     alignItems={"center"}
-  //     w={"full"}
-  //     marginBottom={"10px"}
-  //   >
-  //     <Flex alignItems={"center"} gap={2}>
-  //       <button
-  //         onClick={(e) => {
-  //           setSelectedProduct(item.id);
-  //         }}
-  //       >
-  //         <ProductInfo
-  //           open={selectedProduct === item.id}
-  //           onClose={() => setSelectedProduct(null)}
-  //           item={item}
-  //         />
-  //         <Avatar src={item.imageURL} size={"md"} />
-  //       </button>
-  //       <VStack spacing={2} alignItems={"flex-start"}>
-  //         <button
-  //           onClick={(e) => {
-  //             setSelectedProduct(item.id);
-  //           }}
-  //         >
-  //           {/* {console.log(item)} */}
-  //           <Box fontSize={12} fontWeight={"bold"}>
-  //             {item.name}
-  //           </Box>
-  //         </button>
-  //         <Box fontSize={11} color={"gray.500"}>
-  //           {item.buyNowPrice} $
-  //         </Box>
-  //       </VStack>
-  //     </Flex>
-  //     {/* {setItem([])} */}
-  //   </Flex>
-  // ));
+  // Iterate over each item in the items array and render its details
+  const renderedItems = items.map((item) => (
+    <Flex
+      key={item.id}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      w={"full"}
+      marginBottom={"10px"}
+    >
+      <Flex alignItems={"center"} gap={2}>
+        <button
+          onClick={(e) => {
+            setSelectedProduct(item.id);
+          }}
+        >
+          <ProductInfo
+            open={selectedProduct === item.id}
+            onClose={() => setSelectedProduct(null)}
+            item={item}
+          />
+          <Avatar src={item.imageURL} size={"md"} />
+        </button>
+        <VStack spacing={2} alignItems={"flex-start"}>
+          <button
+            onClick={(e) => {
+              setSelectedProduct(item.id);
+            }}
+          >
+            {/* {console.log(item)} */}
+            <Box fontSize={12} fontWeight={"bold"}>
+              {item.name}
+            </Box>
+          </button>
+          <Box fontSize={11} color={"gray.500"}>
+            {item.buyNowPrice} $
+          </Box>
+        </VStack>
+      </Flex>
+      {/* {setItem([])} */}
+    </Flex>
+  ));
 
   return (
     // Render the array of items
-    // <>{renderedItems}</>
-    <></>
+    <>{renderedItems}</>
   );
 };
 
