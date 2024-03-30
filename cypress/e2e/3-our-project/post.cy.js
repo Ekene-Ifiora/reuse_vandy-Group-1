b117a99dd59f33f7289d5ca7e32d584ac270906a
MarcusKamen <marcus.p.kamen@vanderbilt.edu>
2024-03-30 16:55:59 -0500

    more testing

---
+++ cypress/e2e/3-our-project/post.cy.js
@@ -0,0 +1,84 @@
+describe('post tests', () => {
+    it('passes', () => {
+      cy.visit('/')
+    });
+
+    /* ==== Generated with Cypress Studio ==== */
+    it('failed', () => {
+        cy.visit('/');
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').clear('j');
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').type('johndoe@gmail.com');
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').clear();
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').type('Password{enter}');
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > .sc-gLLuof').click();
+        cy.wait(2000);
+        cy.get('.css-fxrfob > .nav-icons').click();
+        cy.get('[placeholder="Item Name"]').clear('ch');
+        cy.get('[placeholder="Item Name"]').type('ch');
+        cy.get('[placeholder="Description"]').clear('h');
+        cy.get('[placeholder="Description"]').type('h');
+        cy.get(':nth-child(3) > .chakra-select').select('Furniture');
+        cy.get(':nth-child(4) > .chakra-select').select('Rothschild');
+        cy.get('.css-mjjomy').clear('9');
+        cy.get('.css-mjjomy').type('9');
+        cy.get('#chakra-modal--body-\\:rf\\: > [stroke="currentColor"]').click();
+        cy.get('.chakra-button').click();
+        cy.get('#toast-1-title').click();
+        cy.get('.css-1pq15d > .chakra-icon > path').click();
+        cy.get('#chakra-modal--body-\\:rf\\: > [stroke="currentColor"] > path').click();
+        cy.get('.css-7ks4pj').click();
+        /* ==== End Cypress Studio ==== */
+    });
+
+    /* ==== Generated with Cypress Studio ==== */
+    it('upload and eliminate', () => {
+        cy.visit('/');
+        cy.get('.css-fxrfob > .nav-icons').click();
+        cy.get('[placeholder="Item Name"]').clear('ch');
+        cy.get('[placeholder="Item Name"]').type('ch');
+        cy.get('[placeholder="Description"]').clear('h');
+        cy.get('[placeholder="Description"]').type('h');
+        cy.get(':nth-child(3) > .chakra-select').select('Furniture');
+        cy.get(':nth-child(4) > .chakra-select').select('Rothschild');
+        cy.get('.css-mjjomy').clear('9');
+        cy.get('.css-mjjomy').type('9');
+        cy.get('.css-1cjy4zv').attachFile('profile.png');
+        /* ==== Generated with Cypress Studio ==== */
+        cy.get('.css-1oqedzn > .chakra-icon > path').click();
+        /* ==== End Cypress Studio ==== */
+    });
+
+    /* ==== Generated with Cypress Studio ==== */
+    it('pass', () => {
+        cy.visit('/');
+        cy.get('.logout > .nav-icons').click();
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').clear('j');
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').type('johndoe@gmail.com');
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').clear();
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').type('Password{enter}');
+        cy.get('.sc-dmyCSP > .sc-hLQSwg > .sc-gLLuof').click();
+        cy.wait(2000);
+        cy.get('.css-fxrfob > .nav-icons').click();
+        cy.get('[placeholder="Item Name"]').clear('ch');
+        cy.get('[placeholder="Item Name"]').type('ch');
+        cy.get('[placeholder="Description"]').clear('h');
+        cy.get('[placeholder="Description"]').type('h');
+        cy.get(':nth-child(3) > .chakra-select').select('Furniture');
+        cy.get(':nth-child(4) > .chakra-select').select('Rothschild');
+        cy.get('.css-mjjomy').clear('9');
+        cy.get('.css-mjjomy').type('9');
+        cy.get('.css-1cjy4zv').attachFile('profile.png');
+        /* ==== End Cypress Studio ==== */
+        /* ==== Generated with Cypress Studio ==== */
+        cy.wait(1000);
+        cy.get('.chakra-button').click();
+        cy.get('#toast-1-description').click();
+        cy.get('.css-1pq15d > .chakra-icon').click();
+        cy.get('.profile-container > .nav-icons').click();
+        cy.get(':nth-child(1) > .css-1rmkh0t > .css-1cp8gch').click();
+        cy.get('.chakra-icon > path').click();
+        cy.get(':nth-child(1) > .css-1rmkh0t > .css-1cp8gch').click();
+        cy.get('[d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"]').click();
+        /* ==== End Cypress Studio ==== */
+    });
+});
--- cypress/e2e/3-our-project/profile.cy.js
+++ cypress/e2e/3-our-project/profile.cy.js
@@ -19,4 +19,49 @@ describe('profile tests', () => {
     cy.get('.css-mpjpv5 > .css-bk9fzy > svg').click();
     /* ==== End Cypress Studio ==== */
   });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Edit Profile Text', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('/');
+    cy.get('.logout > .nav-icons').click();
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').clear('j');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').type('johndoe@gmail.com');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').clear();
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').type('Password{enter}');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > .sc-gLLuof').click();
+    cy.wait(2000);
+    cy.get('.profile-container > .nav-icons').click();
+    cy.get('.chakra-button').click();
+    cy.get('[placeholder="Full Name"]').type('1');
+    cy.get('[placeholder="Username"]').type('1');
+    cy.get('[placeholder="Bio"]').type('1');
+    cy.get('.css-1bdxafm').click();
+    cy.get('.css-1wbg2w3').click();
+    cy.get('[placeholder="Full Name"]').type('{backspace}');
+    cy.get('[placeholder="Username"]').type('{backspace}');
+    cy.get('[placeholder="Bio"]').type('{backspace}');
+    cy.get('.css-1bdxafm').click();
+    /* ==== End Cypress Studio ==== */
+  });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Edit Profile Picture', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('/');
+    cy.get('.logout > .nav-icons').click();
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').clear('j');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="email"]').type('johndoe@gmail.com');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').clear();
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').type('Password{enter}');
+    cy.get('.sc-dmyCSP > .sc-hLQSwg > .sc-gLLuof').click();
+    cy.wait(2000);
+    cy.get('.profile-container > .nav-icons').click();
+    cy.get('.chakra-button').click();
+    cy.get('.css-1agxkw4').click();
+    cy.get('.chakra-button').click();
+    cy.get('.css-1jif775 > .chakra-button').attachFile('profile.png');
+    cy.get('.css-1bdxafm').click();
+    /* ==== End Cypress Studio ==== */
+  });
 });
