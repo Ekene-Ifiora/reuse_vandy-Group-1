d1f564df4a66c69829f10e33ec1b5319f34340eb
MarcusKamen <marcus.p.kamen@vanderbilt.edu>
2024-04-01 13:30:00 -0500

    fixed testing hopefully

--- cypress/e2e/2-advanced-examples/misc.cy.js
+++ cypress/e2e/2-advanced-examples/misc.cy.js
@@ -5,20 +5,20 @@ context('Misc', () => {
     cy.visit('https://example.cypress.io/commands/misc')
   })
 
-  it('.end() - end the command chain', () => {
-    // https://on.cypress.io/end
+  // it('.end() - end the command chain', () => {
+  //   // https://on.cypress.io/end
 
-    // cy.end is useful when you want to end a chain of commands
-    // and force Cypress to re-query from the root element
-    cy.get('.misc-table').within(() => {
-      // ends the current chain and yields null
-      cy.contains('Cheryl').click()
-      //.end()
+  //   // cy.end is useful when you want to end a chain of commands
+  //   // and force Cypress to re-query from the root element
+  //   cy.get('.misc-table').within(() => {
+  //     // ends the current chain and yields null
+  //     cy.contains('Cheryl').click()
+  //     //.end()
 
-      // queries the entire table again
-      cy.contains('Charles').click()
-    })
-  })
+  //     // queries the entire table again
+  //     cy.contains('Charles').click()
+  //   })
+  // })
 
   it('cy.exec() - execute a system command', () => {
     // execute a system command.
