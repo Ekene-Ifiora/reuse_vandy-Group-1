702d2c3726dc0fdee842f80025266aca0a7377ca
MarcusKamen <marcus.p.kamen@vanderbilt.edu>
2024-04-15 17:47:50 -0500

    fix auction bug

--- cypress/e2e/3-our-project/auction.cy.js
+++ cypress/e2e/3-our-project/auction.cy.js
@@ -13,22 +13,24 @@ describe('auction tests', () => {
     cy.get('.sc-dmyCSP > .sc-hLQSwg > [type="password"]').type('Password');
     cy.get('.sc-dmyCSP > .sc-hLQSwg > .sc-gLLuof').click();
     cy.wait(2000);
-    cy.get(':nth-child(1) > .card_img > .img').click();
+    cy.get(':nth-child(4) > .card_img > .img').click();
     cy.get('.css-ldoqu4 > .chakra-stack > :nth-child(2)').click();
-    cy.get('#chakra-modal-\\:r1h\\: > .chakra-modal__close-btn > .chakra-icon').click();
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get(':nth-child(2) > .chakra-stack > .chakra-button').click();
+    cy.wait(2000);
+    cy.get('.home > .nav-icons').click();
+    cy.wait(2000);
+    cy.get(':nth-child(4) > .card_img > .img').click();
     cy.get('.css-ldoqu4 > .chakra-stack > :nth-child(1)').click();
+    cy.get('.css-1xt0hpo').clear('0');
+    cy.get('.css-1xt0hpo').type('0');
     cy.get('.css-f2hjvb').click();
     cy.get('.css-ldoqu4 > .chakra-stack > :nth-child(1)').click();
-    cy.get('#field-\\:r21\\:').clear('0');
-    cy.get('#field-\\:r21\\:').type('0');
+    cy.get('.css-1xt0hpo').clear('0');
+    cy.get('.css-1xt0hpo').type('0');
     cy.get('.css-wubf5g').click();
-    cy.get(':nth-child(4) > #chakra-toast-manager-bottom > .chakra-toast > .chakra-toast__inner > #toast-1 > .css-njbp03').click();
-    cy.get('.css-ldoqu4 > .chakra-stack > :nth-child(1)').click();
-    cy.get('#field-\\:r2f\\:').clear('0');
-    cy.get('#field-\\:r2f\\:').type('0');
-    cy.get('.css-wubf5g').click();
-    cy.get('.css-ldoqu4 > .chakra-stack > :nth-child(2)').click();
-    cy.get('#chakra-modal--body-\\:r1h\\: > .css-o51fhk > :nth-child(3) > .chakra-stack > .chakra-button').click();
+    cy.get(':nth-child(4) > #chakra-toast-manager-bottom > .chakra-toast > .chakra-toast__inner > #toast-1 > .css-njbp03 > #toast-1-title').click();
+    cy.get(':nth-child(4) > #chakra-toast-manager-bottom > .chakra-toast > .chakra-toast__inner > #toast-1 > .css-1pq15d > .chakra-icon').click();
     /* ==== End Cypress Studio ==== */
   });
 });
